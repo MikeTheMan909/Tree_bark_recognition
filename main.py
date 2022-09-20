e86201337a95a7e86291e0ff6b43d74912fa7c20
Mike Oudshoorn <mike@spectral.energy>
2022-09-20 09:53:49 +0200

    added an gitignore

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ if __name__ == '__main__':
     else:
         path_to_foto = 'fotos/Boom_1_A.jpg'
 
-    imS = cv2.imread(path_to_foto)              #For opening image
-    img = cv2.resize(imS, (640, 480))           #Resize to smaller size for easy screen
+    img = cv2.imread(path_to_foto)              #For opening image
+   # img = cv2.resize(imS, (640, 480))           #Resize to smaller size for easy screen
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  #HSV filter
     gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    color = cv2.applyColorMap(img, cv2.COLORMAP_JET)
+    invert = cv2.bitwise_not(img)
+    color = cv2.applyColorMap(invert, cv2.COLORMAP_JET)
 
     cv2.imshow('color',color)
     cv2.imshow("img",img)                       #Output original
